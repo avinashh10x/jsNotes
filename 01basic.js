0a9fa46fe23cad72356c706fbe9565935cb22129
Avinash <raavaannn@gmail.com>
2025-02-27 17:29:30 +0530

    first commit

---
+++ 01basic.js
@@ -0,0 +1,100 @@
+// programming language is used to data storage and managing data
+
+// data is store is variable
+
+// var
+// let
+// const
+
+// {
+//     // scope is space between curly brackets
+// }
+
+
+// var global level
+// let , const block level
+
+
+// var name = "Rahul";
+
+// let sername = "verma";
+// const age = 20;
+
+// sername = "gutpa"
+
+// console.log(myage);
+// console.log(name);
+// console.log(sername);
+// console.log(age);
+
+
+
+// types in data
+
+// premitive and non-premitive
+
+
+// let student = {
+//     name: "rahul",
+//     age: 20,
+// }
+
+// console.log(student);
+
+// student.age = 50
+
+// console.log(student);
+
+// let name = new String("novem")
+
+let name = "rahul"; // string
+let age = 0; // number || value got to 2^53
+let isStudent = true; // boolean
+let isEmployee // undefined
+let isNull = null; // null mean empty !0
+let bigInt = 123456n; // big int
+// nan not a number
+
+
+
+let numberIsEmplee = Number(isEmployee)
+let numberisnull = Number(isNull)
+let numberIsStundet = Number(isStudent)
+
+// console.log(numberIsStundet);
+// console.log(numberisnull);
+
+// console.log(numberIsEmplee);
+
+
+
+// console.table([name, age, isStudent, isEmployee, isNull, bigInt]);
+// console.table([typeof (name), typeof (age), typeof (isStudent), typeof (isEmployee), typeof (isNull), typeof (bigInt)]);
+
+
+// let rollno = "22hh"
+// let numberRollNumber = Number(rollno)
+
+// let stringAge = String(age)
+
+// let srtingIsnull = String(isNull)
+// // console.log(srtingIsnull)
+
+// // console.log(numberRollNumber)
+// // console.log(stringAge)
+
+
+// let booleanAge = Boolean(age)
+
+// console.log(booleanAge)
+
+
+// console.log(1 + 1)
+// console.log(1 - 1)
+
+
+
+
+// to use github
+// your system should has git
+//  go to github and create a new repository // repo is a folder
---
+++ 02operations.js
@@ -0,0 +1,66 @@
+// operations in js
+
+let value = 5;
+let negValue = -value
+
+// value++ // post increment +1
+//  ++value // pre increment +1
+
+value-- // post decrement -1
+
+// console.log(value)
+
+// console.log(negValue)
+
+// airtmatic operations 
+// console.log(12 + 5)
+// console.log(12 - 5)
+// console.log(12 / 5)
+// console.log(12 * 5)
+// console.log(12 % 5) //remainder
+// console.log(12 ** 5) //12^5
+
+// concatanation operations
+
+let str1 = "Hello"
+let str2 = "World"
+
+let str3 = str1 + str2
+
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+// console.log(1 + "2" + 3)
+
+
+// console.log(null - 1)
+
+
+
+// console.log(str1 + " " + str2)
+
+// comparison operations
+
+// console.log("2">1)
+// console.log(null>0)
+// console.log(null <0)
+// console.log(null == 0)
+
+// console.log(null == undefined)
+// console.log(undefined > null)
+// console.log(undefined < null)
+
+// console.log(undefined == 0)
+
+
+
+// =
+// == // used to  comaparison 
+// === // used to comaparison but also check datatype 
+
+A = 5   //  =  value is assinged to our variable A
+B = "5"
+
+// console.log(A == B)
+
+
+console.log(A === B)
