e2e40f8222a162bf84d976bf91a070034fbd00ff
Avinash <raavaannn@gmail.com>
2025-03-04 17:14:45 +0530

    Add initial implementations for string and number manipulations

---
+++ String.js
@@ -0,0 +1,83 @@
+
+let name = "Novemcontrols"
+// n-0
+// o-1
+// v-2
+
+// console.log(name.substring(0,2))
+// console.log(name.slice(-1,11))
+// console.log(name)
+
+let SecondName = name.trim()
+// console.log(name)
+// console.log(name.trim())
+
+// string is collaction of characters which are written in backticks or double quotes or single quotes
+
+let guy = "rahul"
+const age = 60
+
+
+// console.log(guy + " is " + age + " years old")
+// console.log(`${guy} is ${age} years old`)
+
+
+
+
+// console.log(name.length);
+
+// console.log(name[1]);
+
+// let firstChar = name[0]
+// console.log(firstChar);
+
+// console.log(name.indexOf('s'))
+
+
+// string manupulation
+
+// console.log(SecondName.toUpperCase())
+
+
+let bio = "my name is mandeep"
+let bio2 = bio.replace("mandeep", "rahul")
+let bio3 = bio.replaceAll("a", "@")
+
+// console.log(bio3.includes("is"));
+
+console.log(typeof(NaN))
+
+
+// console.log(bio2.search("mandeep"))
+
+// console.log(bio3)
+
+// console.log(bio.split(" "))
+// console.log(SecondName.split(""))
+
+let arr = ['my', 'name', 'is', 'mandeep']
+
+
+// console.log(arr.join("    "))
+
+// console.log(SecondName.repeat(2))
+
+let str1 = "aman"
+let str2 = "daman"
+// console.log(str1.charAt(0));
+
+
+// console.log(str1.concat(str2))
+
+
+
+// let value:String = "javascript"
+// console.log(value.slice(1,3))
+
+
+
+// console.log("mohan's age is: - ", age, "and im old")
+
+// console.log(`mohan's age is: - ${age} and im old`)
+
+const num = 0xff
---
+++ number.js
@@ -0,0 +1,44 @@
+// convertions on numbers
+
+let stringValue = "10"
+let stringValue2 = "hello"
+// console.log(Number(stringValue))
+// console.log(Number(stringValue2))
+
+let value = undefined
+// console.log(Number(value))
+let value2 = null
+// console.log(Number(value2))
+
+let numberValue = -Infinity
+
+// console.log(numberValue)
+
+// console.log(Number.isNaN(5))
+
+
+
+
+let numStringMix = "45"
+// console.log(parseInt("455.999"))
+let numStringMix2 = "jj45"
+// console.log(parseFloat("55.55pf"))
+
+// console.log(parseInt("100px"))
+
+// console.log(9.toLocaleString('ar-EG'))
+
+
+let number = 9.124578963
+// console.log(number.valueOf())
+// console.log(number.toFixed(2))
+// console.log(number.toExponential())
+// console.log(number.toLocaleString('ar-EG'))
+
+
+console.log("5" + 4)
+console.log("5" - 4)
+console.log(true + 1)
+
+
+// () try to convert both diff data types into a same data type
